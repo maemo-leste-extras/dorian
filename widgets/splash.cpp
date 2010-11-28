d472b48485a397a24a3e4b1f5f6f2725b191ed40
Akos Polster <akos@pipacs.com>
2010-11-28 17:16:21 +0100

    Even more splash screen polishing.

--- widgets/splash.cpp
+++ widgets/splash.cpp
@@ -2,7 +2,14 @@
 
 #include "splash.h"
 
-Splash::Splash(): QSplashScreen(QPixmap(":/icons/splash.jpg").scaled(360, 518,
-                      Qt::IgnoreAspectRatio, Qt::SmoothTransformation))
+Splash::Splash(): QSplashScreen()
 {
+    QRect geo = QApplication::desktop()->availableGeometry();
+    if (geo.width() < geo.height()) {
+        setPixmap(QPixmap(":/icons/splash.jpg").scaled(360, 518,
+            Qt::IgnoreAspectRatio, Qt::SmoothTransformation));
+    } else {
+        setPixmap(QPixmap(":/icons/splash-landscape.jpg").scaled(520, 330,
+            Qt::IgnoreAspectRatio, Qt::SmoothTransformation));
+    }
 }
